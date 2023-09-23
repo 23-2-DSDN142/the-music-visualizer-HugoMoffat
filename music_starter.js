ac9a8c1bd23c769d4f53c9cbde76d2947cad006e
HugoMoffat <kibb456@gmail.com>
2023-09-23 12:09:13 +1200

    orbs of scrying

--- music_starter.js
+++ music_starter.js
@@ -13,31 +13,31 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
    // vocal bar is red
    fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+   ellipse(bar_pos_x, height / 2 - 3 * bar_spacing, 4 * vocal, 4 * vocal);
    fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+   //text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
    // drum bar is green
    fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+   ellipse(bar_pos_x, height / 2 - 1 * bar_spacing, 4 * drum, 4 * drum);
    fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+   //text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
    // bass bar is blue
    fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
+   ellipse(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * bass, 4 * bass);
    fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
+   //text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
  
    // other bar is white
    fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
+   ellipse(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * other, 4 * other);
    fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
+   //text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
    fill(255, 255, 0);
  
    // display "words"
    textAlign(CENTER);
    textSize(vocal);
-   text(words, width/2, height/3);
+   text(words, width/2, height/2);
 }
