21f12f05b95d23b0aadbda8e9e36e9bfa9df575a
HugoMoffat <kibb456@gmail.com>
2023-09-25 12:11:39 +1300

    forming scene

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,53 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  background(50)
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+   noStroke()
+   
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   ellipse(bar_pos_x, height / 2 - 3 * bar_spacing, 4 * vocal, 4 * vocal);
-   fill(0);
-   //text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   ellipse(bar_pos_x, height / 2 - 1 * bar_spacing, 4 * drum, 4 * drum);
-   fill(0);
-   //text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   ellipse(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * bass, 4 * bass);
-   fill(0);
-   //text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   ellipse(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * other, 4 * other);
-   fill(0);
-   //text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
+   fill(50, 150, 240);
+   ellipse(500, 700, 4 * vocal, 4 * vocal);
+   
+   fill(255, 125)
+   ellipse(500, 700, 400 + vocal, 400 + vocal) 
+
+   fill(227, 81, 41);
+   ellipse(750, 350, 2 * drum, 2 * drum);
+   ellipse(500, 150, 2 * bass, 2 * bass);
+   ellipse(150, 200, 2 * other, 2 * other);
+   
+   fill(0)
+   rect(750, 350, 5, 10)
+   rect(500, 150, 5, 10)
+   rect(150, 200, 5, 10)
+   
+   fill(255, 187, 138)
+   rect(750, 390, 40, 80)
+   rect(500, 190, 40, 80)
+   rect(150, 240, 40, 80)
+   
+   fill(186, 119, 71)
+   rect(763, 388, 9, 70)
+   rect(137, 240, 7, 70)
+   rect(517, 188, 5, 70)
+   rect(483, 188, 5, 70)
+
+   fill(30)
+   rect(750, 455, 90, 50)
+   rect(500, 255, 90, 50)
+   rect(150, 305, 90, 50)
+
+   
+
+
+
+
    // display "words"
    textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/2);
+   //textSize(vocal);
+   //text(words, width/2, 550);
 }
